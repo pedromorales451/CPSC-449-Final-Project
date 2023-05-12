802b8edab1e621328bb07252f1ad49b3c121c85f
Kim Eaton <60245827+XCaliCatX@users.noreply.github.com>
2023-05-12 11:42:51 -0700

    updateby id, get by id

--- bookstore/main.py
+++ bookstore/main.py
@@ -20,26 +20,55 @@ class Book(BaseModel):
     price: float
     stock: int
     numberOfSales: int
+    book_id: int
+
 
 @app.get("/")
-def index():
+async def index():
     return {'greeting': 'Welcome to Bookstore'}
 
+#●	GET /books: Retrieves a list of all books in the store
 @app.get("/books")
 async def books() -> List[Book]:
     result = await collection.find().to_list(length=100)
         
     return result
 
+#●	GET /books/{book_id}: Retrieves a specific book by ID
+@app.get("/books/{book_id}")
+async def getbook(book_id: int) -> List[Book]:
+    if(book_id >=0 and book_id <=1):
+        result = await collection.find({"book_id": book_id}).to_list(length=100)
+    else: 
+        result=[]
+    return result
 
+#●	POST /books: Adds a new book to the store  
 @app.post("/create-book/")
 async def create_book(book: Book):
     try:
         # perform insert_one() asynchronously, 
         # convert book to dict before insertion
-        result = await collection.insert_one(dict(book))
+        result = await db.collection.insert_one(dict(book))
 
         # return the inserted object id 
         return {"inserted_id": str(result.inserted_id)}
     except DuplicateKeyError:
         return {"error": "Duplicate key error!"}
+
+
+
+
+#●	PUT /books/{book_id}: Updates an existing book by ID
+@app.put("/books/{book_id}")
+async def update_book(book_id:int)->List[Book]:
+    collection.update_one({"book_id": book_id}, {"$set":{"stock":4}})
+    result = await collection.find({"book_id": book_id}).to_list(length=100)
+    return result
+
+#●	DELETE /books/{book_id}: Deletes a book from the store by ID
+#●	GET /search?title={}&author={}&min_price={}&max_price={}: Searches for books by title, author, and price range
+
+@app.get("/search?title={}&author={}&min_price={}&max_price={}: Searches for books by title, author, and price range")
+async def search():
+    return []
